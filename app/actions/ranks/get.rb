3d98b54d2ff70a149ff17cf9f82e573bc908b781
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-04-11 14:34:48 -0700

    fetch players

--- app/actions/ranks/get.rb
+++ app/actions/ranks/get.rb
@@ -5,26 +5,37 @@ module Ranks
     extend LightService::Action
 
     expects :client, default: Warpcast::Client.new
+    expects :periods_ago, default: nil
     expects :page, default: 1
-    expects :winners, default: []
+    promises :next_page
 
     executed do |context|
-      cursor = self.cursor(context.page)
-      url = "/v1/creator-rewards-winner-history?cursor=#{cursor}"
+      cur = cursor(context.page)
+      url = "/v1/creator-rewards-winner-history?cursor=#{cur}"
+      if context.periods_ago.present?
+        url += "&periodsAgo=#{context.periods_ago}"
+      end
       response = context.client.get(url)
       response['result']['history']['winners'].each do |winner|
-        if Player.exists?(uid: winner['fid'].to_i)
-          player = Player.find_by(uid: winner['fid'].to_i)
-          player.score << winner['score']
-          player.save!
-          context.winners << player
+        player = Player.find_or_initialize_by(uid: winner['fid'].to_i).tap do |player|
+          player.username = winner['username']
         end
+        player.score << winner['score']
+        player.save!
       end
+      context.next_page = next_page(response.dig('next', 'cursor'))
     end
 
     def self.cursor(page)
       params = { page:, limit: 100 }
       Base64.strict_encode64(params.to_json).chomp('=')
+    end
+
+    def self.next_page(cursor)
+      return nil if cursor.blank?
+
+      params = JSON.parse(Base64.decode64(cursor))
+      params['page']
     end
   end
 end
--- app/jobs/fetch_rank_job.rb
+++ app/jobs/fetch_rank_job.rb
@@ -8,7 +8,7 @@ class FetchRankJob < ApplicationJob
     25.times do |i|
       new_result = Ranks::Get.execute(client:, page: i + 1)
       sleep 1
-      break if !new_result.success?
+      break if !new_result.success? || new_result.next_page.blank?
     end
   end
 end
