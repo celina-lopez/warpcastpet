9ef1af05c415257c2d51dc125520beb7803dd147
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-04-15 15:04:07 -0700

    some stuff removed

--- app/javascript/components/addframe.jsx
+++ app/javascript/components/addframe.jsx
@@ -1,31 +1,14 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import {
     AddFrame
 } from "@farcaster/frame-sdk";
 
 export default function AddToWarpcastFrame({context, added, actions}) {
-  const [notificationDetails, setNotificationDetails] =
-    useState(null);
   const [addFrameResult, setAddFrameResult] = useState("");
-
-  useEffect(() => {
-    setNotificationDetails(context?.client.notificationDetails ?? null);
-  }, [context]);
 
   const addFrame = useCallback(async () => {
     try {
-      setNotificationDetails(null);
-
-      const result = await actions.addFrame();
-
-      if (result.notificationDetails) {
-        setNotificationDetails(result.notificationDetails);
-      }
-      setAddFrameResult(
-        result.notificationDetails
-          ? `Added, got notificaton token ${result.notificationDetails.token} and url ${result.notificationDetails.url}`
-          : "Added, got no notification details"
-      );
+      await actions.addFrame();
     } catch (error) {
       if (error instanceof AddFrame.RejectedByUser) {
         setAddFrameResult(`Not added: ${error.message}`);
@@ -40,29 +23,15 @@ export default function AddToWarpcastFrame({context, added, actions}) {
   }, []);
 
   return (
-    <div className="w-[300px] mx-auto py-4 px-2">
-      <div>
-        <div>
-          <div className="mt-2 mb-4 text-sm">
-            Client fid {context?.client.clientFid},
-            {added ? " frame added to client," : " frame not added to client,"}
-            {notificationDetails
-              ? " notifications enabled"
-              : " notifications disabled"}
-          </div>
-
-          <div className="mb-4">
+    <div className="mb-4">
             {addFrameResult && (
               <div className="mb-2 text-sm">
                 Add frame result: {addFrameResult}
               </div>
             )}
-            <button onClick={addFrame} disabled={added} className="bg-blue-500 text-white px-4 py-2 rounded-md">
+            <button onClick={addFrame} className="bg-blue-500 text-white px-4 py-2 rounded-md">
               Add frame to client
             </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- app/jobs/fetch_rank_job.rb
+++ app/jobs/fetch_rank_job.rb
@@ -3,11 +3,12 @@
 class FetchRankJob < ApplicationJob
   queue_as :default
 
-  def perform(periods_ago)
+  def perform(periods_ago = nil)
     client = Warpcast::Client.new
     25.times do |i|
       new_result = Ranks::Get.execute(client:, page: i + 1, periods_ago:)
-      sleep 1
+      sleep 2
+      puts "page #{i + 1} done"
       break if !new_result.success? || new_result.next_page.blank?
     end
   end
